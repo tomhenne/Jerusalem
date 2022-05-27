97f7c2d7e943aa1555ea518e583260385c2cbb4f
Thomas Henne <th@esymetric.de>
2022-05-27 22:22:14 +0200

    test order

--- src/test/java/rebuilding/RebuilderTest.kt
+++ src/test/java/rebuilding/RebuilderTest.kt
@@ -1,24 +1,31 @@
 package rebuilding
 
 import de.esymetric.jerusalem.osmDataRepresentation.OSMDataReader
+import de.esymetric.jerusalem.ownDataRepresentation.geoData.Position
 import de.esymetric.jerusalem.rebuilding.Rebuilder
 import de.esymetric.jerusalem.routing.Router
 import de.esymetric.jerusalem.routing.Router.Companion.debugMode
+import de.esymetric.jerusalem.routing.RoutingType
 import de.esymetric.jerusalem.routing.algorithms.TomsAStarStarRouting
 import de.esymetric.jerusalem.routing.heuristics.TomsRoutingHeuristics
+import de.esymetric.jerusalem.tools.CrossroadsFinder
 import de.esymetric.jerusalem.utils.BufferedRandomAccessFile.Companion.shortInfoAndResetCounters
 import org.apache.tools.bzip2.CBZip2InputStream
+import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import routing.RouterTest
 import java.io.File
 import java.io.FileInputStream
 import java.util.*
+import kotlin.test.assertEquals
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class RebuilderTest {
-    var routerTest = RouterTest()
+    private var routerTest = RouterTest()
 
-    val osmTestFilePath = "osmData/munich-hadern-excerpt.osm.bz2"
+    private val osmTestFilePath = "osmData/munich-hadern-excerpt.osm.bz2"
 
     @Test
     @Order(1)
@@ -33,6 +40,8 @@ class RebuilderTest {
             TomsRoutingHeuristics(), false, false, false
         )
         val fis = FileInputStream(osmTestFilePath)
+        fis.read()
+        fis.read()
         val bzis = CBZip2InputStream(fis)
         val reader = OSMDataReader(bzis, rebuilder, false)
         reader.read(Date())
@@ -75,27 +84,16 @@ class RebuilderTest {
         val dataDirectoryPath = "testData"
         val tempDirectoryPath = "tempData"
 
-        // rebuild full
-        var rebuilder = Rebuilder(
-            dataDirectoryPath, tempDirectoryPath,
-            TomsRoutingHeuristics(), false, false, false
-        )
-        var fis = FileInputStream(osmTestFilePath)
-        var bzis = CBZip2InputStream(fis)
-        var reader = OSMDataReader(bzis, rebuilder, false)
-        reader.read(Date())
-        bzis.close()
-        fis.close()
-        rebuilder.finishProcessingAndClose()
-
         // rebuild only ways
-        rebuilder = Rebuilder(
+        val rebuilder = Rebuilder(
             dataDirectoryPath, tempDirectoryPath,
             TomsRoutingHeuristics(), true, true, false
         )
-        fis = FileInputStream(osmTestFilePath)
-        bzis = CBZip2InputStream(fis)
-        reader = OSMDataReader(bzis, rebuilder, true)
+        val fis = FileInputStream(osmTestFilePath)
+        fis.read()
+        fis.read()
+        val bzis = CBZip2InputStream(fis)
+        val reader = OSMDataReader(bzis, rebuilder, true)
         reader.read(Date())
         bzis.close()
         fis.close()
@@ -133,24 +131,13 @@ class RebuilderTest {
         val dataDirectoryPath = "testData"
         val tempDirectoryPath = "tempData"
 
-        // rebuild full
-        var rebuilder = Rebuilder(
-            dataDirectoryPath, tempDirectoryPath,
-            TomsRoutingHeuristics(), false, false, false
-        )
-        val fis = FileInputStream(osmTestFilePath)
-        val bzis = CBZip2InputStream(fis)
-        val reader = OSMDataReader(bzis, rebuilder, false)
-        reader.read(Date())
-        bzis.close()
-        fis.close()
-        rebuilder.finishProcessingAndClose()
+
 
         // rebuild transitions
 
-        rebuilder = Rebuilder(
+        val rebuilder = Rebuilder(
             dataDirectoryPath, tempDirectoryPath,
-            TomsRoutingHeuristics(), false, false, false
+            TomsRoutingHeuristics(), true, false, false
         )
         rebuilder.buildTransitions(true)
 
@@ -176,5 +163,26 @@ class RebuilderTest {
             router, 48.116892, 11.487076, 48.117909, 11.472429,
             "eichenstrasse", dataDirectoryPath
         )
+    }
+
+    @Test
+    @Order(4)
+    fun testCrossroadsFinder() {
+        val dataDirectoryPath = "testData"
+        val crossroadsFinder = CrossroadsFinder(dataDirectoryPath)
+        val positions = listOf(
+            Position().also {
+                it.latitude = 48.115547
+                it.longitude = 11.477455
+            },
+            Position().also {
+                it.latitude = 48.116782
+                it.longitude = 11.484021
+            }
+        )
+        crossroadsFinder.loadNumberOfCrossroads(positions, RoutingType.bike)
+
+        assertEquals(4, positions[0].nrOfTransitions)
+        assertEquals(4, positions[1].nrOfTransitions)
     }
 }
